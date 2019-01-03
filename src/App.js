f4251a4f1882b8a65d4769def64106592a115720
Daniel Merz <MerzDanielDeveloper@gmail.com>
2019-01-03 16:09:06 +0100

    headline

--- src/App.js
+++ src/App.js
@@ -3,22 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state= {
+    path: 'merzdaniel/github_issue-tracker-graphql_playground'
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>IssueTracker</h2>
         </header>
       </div>
     );
