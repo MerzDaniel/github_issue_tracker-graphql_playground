78f367531634dac5fa23b48a29e55395ce7cd75e
Daniel Merz <MerzDanielDeveloper@gmail.com>
2019-01-03 16:23:13 +0100

    show issues

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Issue = issue =>
+  <div className='issue'>
+    <text>({issue.id}) {issue.name}</text>
+  </div>
+
+
 class App extends Component {
-  state= {
-    path: 'merzdaniel/github_issue-tracker-graphql_playground'
+  state = {
+    path: 'merzdaniel/github_issue-tracker-graphql_playground',
+    // issues: [{data: {issues: [{name: 'issue1'}, {name: 'issue2'}] }}],
+    issues: [{name: 'I have nothing to do. Everything is working fine', id: 0}, {name: 'PRODUCTION DOWN HELP!', id: 1}]
   }
 
   render() {
@@ -12,6 +20,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h2>IssueTracker</h2>
+
+          <h3>Issues</h3>
+          <div className="issue_list">
+            {this.state.issues.map(i => Issue(i))}
+          </div>
         </header>
       </div>
     );
